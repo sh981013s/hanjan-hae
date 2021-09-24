790db8ca53237da8e95815d29cc91ab9b6c2f4c1
SeungHwan-Lee <sh981013s@gmail.com>
2021-09-25 00:49:24 +0900

    ✨  Loader added

--- static/js/app.js
+++ static/js/app.js
@@ -4,22 +4,27 @@
 
 (function() {
     
-    const cta = document.querySelector('.cta_header');
-    const footer = document.querySelector(
-    '.toaster')
-    const expandIcon = document.querySelector('.expand_icon')
-    const airplaneIcon = document.querySelectorAll('.airplane_icon_wrapper')
+    // loader
+
+    document.onreadystatechange = function() {
+        const container = document.querySelector('.container');
+        const loader = document.querySelector('.loader');
+
+        console.log(loader);
+
+        if (document.readyState !== "complete") {
+            container.style.display = "none";
+            loader.style.display = "flex";
+        } else {
+            console.log('done');
+            container.style.display = "block";
+            loader.style.display = "none";
+        }
+    };
+
+
+    // typlet
     
-    function toggleFooter() {
-      footer.classList.toggle('is_open');
-      expandIcon.classList.toggle('is_reversed')
-      Array.from(airplaneIcon).map(icon => icon.classList.toggle('is_moving'))
-    }
-    
-    cta.addEventListener('click', toggleFooter);
-
-
-
     (function start() {
         new TypeIt(".sub-title", {
             spee: 50,
